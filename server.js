a851e77a1538bd999cf11e32798151af1abe9a2f
Madhushree <madhushreevs54@gmail.com>
2026-07-22 20:57:51 +0530

    Resolve merge conflict in server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 require("dotenv").config();
 
 // Redis Connection
@@ -38,45 +38,4 @@ connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
-=======
-require("dotenv").config();
-
-// Redis Connection
-require("./src/config/redis");
-
-// Start BullMQ Worker
-require("./src/workers/imageWorker");
-
-const express = require("express");
-const morgan = require("morgan");
-const connectDB = require("./src/config/db");
-const imageRoutes = require("./src/routes/imageRoutes");
-const statusRoutes = require("./src/routes/statusRoutes");
-const resultRoutes = require("./src/routes/resultRoutes");
-const failureRoutes = require("./src/routes/failureRoutes");
-
-
-
-const app = express();
-
-app.use(express.json());
-app.use(morgan("dev"));
-
-app.use("/api", imageRoutes);
-
-app.use("/api", statusRoutes);
-app.use("/api", resultRoutes);
-app.use("/api", failureRoutes);
-
-app.get("/", (req, res) => {
-  res.send("Backend is running...");
-});
-
-const PORT = process.env.PORT || 5000;
-
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
->>>>>>> 4e3bfa66259178a0f834e8fb7a4c5d5d8920aaa9
 });
